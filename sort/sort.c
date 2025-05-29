558a1c7cb0e943dddc9de1a3026ffb2e626a5a96
MarcDevGuy127 <marceloh.github@gmail.com>
2025-05-29 19:10:50 -0300

    Atualizando funções para swap, heapify e heapsort

--- sort/sort.c
+++ sort/sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdio.h>
 
 int compare(const void* a, const void* b)
 {
@@ -44,65 +45,58 @@ void insertionsort(double* arr, long arr_size)
         j=sent;
     }
 }
-// Heap Sort in C
 
-  // Function to swap the the position of two elements
-  //funcao de swap/alternancia entre as posicoes de dois elementos
-  void swap(int *pos_a, int *pos_b) {
-    int temp = *pos_a;
-    *pos_a = *pos_b;
-    *pos_b = temp;
-  }
+// funcao para alternar a posicao de dois elementos(swap)
+void swap(double *a, double *b)
+{
+    double temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-  void heapify(double* arr, long arr_size) {
-    // Find largest among root, left child and right child
-    int i;//
-    int maior = i;
-    int filho_esq = 2 * i + 1;
-    int filho_dir = 2 * i + 2;
+// funcao da filtragem(heapify)
+void heapify(double* arr, long n, long i)
+{
+    double temp;
+    long valor_maximo, posicao_esq, posicao_dir;
 
-    if (filho_esq < arr_size && arr[filho_esq] > arr[maior])
-      maior = filho_esq;
+    valor_maximo = i;
+    posicao_esq = 2 * i + 1;
+    posicao_dir = 2 * i + 2;
 
-    if (filho_dir < arr_size && arr[filho_dir] > arr[maior])
-      maior = filho_dir;
+    if (posicao_esq < n && arr[posicao_esq] > arr[valor_maximo])
+        valor_maximo = posicao_esq;
 
-    // Swap and continue heapifying if root is not largest
-    if (maior != i) {
-      swap(&arr[i], &arr[maior]);
-      heapify(arr, arr_size, maior);
+    if (posicao_dir < n && arr[posicao_dir] > arr[valor_maximo])
+        valor_maximo = posicao_dir;
+
+    if (valor_maximo != i) {
+        temp = arr[i];
+        swap(&arr[i], &arr[valor_maximo]);
+        heapify(arr, n, valor_maximo);
+        //arr[valor_maximo] = temp;
     }
-  }
+}
 
-  // Main function to do heap sort
-  void heapsort(double* arr, long arr_size) {
-    // Build max heap
-    for (int i = arr_size / 2 - 1; i >= 0; i--)
-      heapify(arr, arr_size, i);
+//void heapsort(double* arr, long arr_size)
+void heapsort(double* arr, long arr_size)
+{
+    long i;
+    //double temp;
 
-    // Heap sort
-    for (int i = arr_size - 1; i >= 0; i--) {
-      swap(&arr[0], &arr[i]);
-
-      // Heapify root element to get highest element at root again
-      heapify(arr, i, 0);
+    //construindo max heap
+    for (i = arr_size / 2 - 1; i >= 0; i--) {
+        heapify(arr, arr_size, i);
     }
-  }
 
-  // Print an array
-  void printArray(double* arr, long arr_size) {
-    for (int i = 0; i < arr_size; ++i)
-      printf("%d ", arr[i]);
-    printf("\n");
-  }
+    //heap sort
+    for (i = arr_size - 1; i >= 0; i--) {
+        swap(&arr[0], &arr[i]);
+        //temp = arr[0];
+        //arr[0] = arr[i];
+        //arr[i] = temp;
+        //aplicando heapify na raiz para que o maior elemento volte à raiz. */
 
-  // Driver code
-  int main() {
-    //int arr[] = {1, 12, 9, 5, 6, 10};
-    long int tamanho_array = sizeof(arr) / sizeof(arr[0]);
-
-    heapSort(arr, tamanho_array);
-
-    printf("Sorted array is \n");
-    printArray(arr, tamanho_array);
-  }
+        heapify(arr, i, 0);
+    }
+}
